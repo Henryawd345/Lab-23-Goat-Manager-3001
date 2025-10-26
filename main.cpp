ee73d6324a8bb2bc78f8edb7a19aba7729c724c9
Henryawd345 <henrygoodboy2000@gmail.com>
2025-10-25 23:22:34 -0700

    update task two

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <limits> 
 #include <list>
 #include "Goat.h"
 using namespace std;
@@ -23,13 +24,53 @@ int main() {
     int i = 0;
     while (fin >> names[i++]);
     fin.close();
+
     ifstream fin1("colors.txt");
     string colors[SZ_COLORS];
     i = 0;
     while (fin1 >> colors[i++]);
     fin1.close();
 
+    list<Goat> trip;
 
-    return 0;
+    while (true){
+        int choice = main_menu();
+
+        switch (choice) {
+            case 1:
+
+            cout << "\n[Add goat]\n";
+                break;
+            case 2:
+                cout << "\n[Delete goat]\n";
+                break;
+            case 3:
+                cout << "\n[List goats]n";
+                break;
+            case 4:
+                cout << "\nGoodbye! Thanks for using GOAT MANAGER 3001.\n";
+                return 0;
+
+        }
+    }
 }
+
+int main_menu() {
+    cout << "\n*** GOAT MANAGER 3001 ***\n"
+         << "[1] Add a goat\n"
+         << "[2] Delete a goat\n"
+         << "[3] List goats\n"
+         << "[4] Quit\n"
+         << "Choice --> ";
+
+        int choice;
+
+        while (!(cin >> choice) || choice << 1 || choice > 4){
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Please enter a number between 1 and 4: ";
+        }
+    return choice;
+}
+
 
